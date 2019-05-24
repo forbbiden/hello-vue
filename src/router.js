3d23df000de98e9ece95460d51c399a5482c6dcb
ZhongWen <lizw@primeton.com>
2019-05-24 17:54:01 +0800

    Update

--- src/router.js
+++ src/router.js
@@ -19,6 +19,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/todo",
+      name: "TodoList",
+      component: () => import("./views/TodoList.vue")
     }
   ]
 });
---
+++ src/views/TodoList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h1>This is an TodoList page</h1>
+    <hr/>
+    <div class="todo">
+      添加任务： <input type='text' v-model="inputItem" @keyup.enter="addItem" />
+    </div>
+    <hr/>
+    <ul class="list">
+      <li v-for="(val, index) in list" :key="index" :class="val.completed ? 'green' : (index%2 == 0 ? 'bg1' : 'bg2')" >
+        <input type="checkbox" v-model="val.completed" />
+        {{ val ? val.content : 'undefined' }}
+        <a @click="deleteItem(index)">删除</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "TodoList",
+  methods: {
+    addItem() {
+      if (this.inputItem && this.inputItem.trim().length > 0) {
+        this.list.push({
+          content: this.inputItem,
+          completed: false
+        });
+      }
+      this.inputItem = '';
+    },
+    deleteItem(i) {
+      let ok = confirm(`确认要删除”${this.list[i].content}“吗？`);
+      if (ok) {
+        this.list.splice(i, 1);
+      }
+    }
+  },
+  data: function() {
+    return {
+      inputItem: '',
+      list: []
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo {
+  text-align: left;
+  color: green;
+  font-size: 120%;
+}
+.todo input {
+  font-size: 120%;
+}
+.list {
+  text-align: left;
+}
+.list a {
+  margin-left: 100px;
+}
+
+.bg1 {
+  background-color: darkgrey;
+}
+.bg2 {
+  background-color: darkkhaki;
+}
+
+.green {
+  background-color: green;
+}
+</style>
