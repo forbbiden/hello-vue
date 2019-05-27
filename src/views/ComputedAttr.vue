f1def2998854590423048bd750810d43d45dd149
ZhongWen <lizw@primeton.com>
2019-05-27 17:39:03 +0800

    Update

--- src/views/ComputedAttr.vue
+++ src/views/ComputedAttr.vue
@@ -4,19 +4,37 @@
     <p>Original message: "{{ message }}"</p>
     <p>Computed reversed message: "{{ reversedMessage }}"</p>
     <input v-model="message" type="text" />
+    <p>计算属性：{{ nowOfComputed }}</p>
+    <p>计算属性：{{ nowOfComputed }}</p>
+    <p>方法：{{ nowOfMethod() }}</p>
+    <p>方法：{{ nowOfMethod() }}</p>
   </div>
 </template>
 <script>
+import { setInterval } from 'timers';
 export default {
   name: "ComputedAttr",
   data: function() {
     return {
-      message: "Hello Vue !"
+      message: "Hello Vue !",
+      now: Date.now()
     };
   },
   computed: {
     reversedMessage: function() {
       return this.message.split('').reverse().join('') // 消息内容反转
+    },
+    nowOfComputed: function() {
+      // 在now不变的情况下，该方法只会执行一次，Vue进行了缓存处理
+      console.log("computed#now");
+      return this.now;
+    }
+  },
+  methods: {
+    nowOfMethod() {
+      // 每次都会调用
+      console.log("method#now");
+      return this.now;
     }
   }
 }
