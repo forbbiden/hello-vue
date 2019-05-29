6f1cdd5da899eafab8d4e4ce579f25e229931df7
ZhongWen <lizw@primeton.com>
2019-05-29 16:12:55 +0800

    Update

--- src/views/Style.vue
+++ src/views/Style.vue
@@ -1,7 +1,17 @@
 <template>
-  <div :class="{ active: isActive, active2: isActive }">
-    Vue绑定HTML class 
-    <input type="button" :value="isActive ? '禁用样式' : '启用样式'" @click="changeActive" />
+  <div>
+    <div class="big-font" :class="{ active: isActive, active2: isActive }">
+      Vue绑定HTML class 
+      <input type="button" :value="isActive ? '禁用样式' : '启用样式'" @click="changeActive" />
+    </div>
+    <hr>
+    <div :class="classObject">
+      绑定的数据对象不必内联定义在模板里： v-bind:class="classObject"
+    </div>
+    <hr>
+    <div :class="computedClass">
+      渲染的结果和上面一样。我们也可以在这里绑定一个返回对象的计算属性。这是一个常用且强大的模式：
+    </div>
   </div>
 </template>
 <script>
@@ -9,18 +19,30 @@ export default {
   name: "StyleTest",
   data: function() {
     return {
-      isActive: true
+      isActive: true,
+      classObject: ["active", "active2", "big-font"]
     }
   },
   methods: {
     changeActive() {
       this.isActive = !this.isActive;
     }
+  },
+  computed: {
+    computedClass: function() {
+      return {
+        active: this.isActive,
+        "big-font": true
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
+.big-font {
+  font-size: larger;
+}
 .active {
   color: darkgreen;
   font-weight: bold;
