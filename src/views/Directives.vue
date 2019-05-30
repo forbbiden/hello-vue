243076da8ba648c3f24244263bffce941f96ced7
ZhongWen <lizw@primeton.com>
2019-05-30 10:30:52 +0800

    Update

--- src/views/Directives.vue
+++ src/views/Directives.vue
@@ -29,6 +29,7 @@
       <br>
       <hr>
 
+      <h1>Directives: v-key</h1>
       <a @click="loginTypeSwitch" :style="{ color: 'green', fontSize: '20px', 'padding-bottom': '40px'}">切换登录方式</a>
       <hr>
       <template v-if="loginType === 'username'">
@@ -54,6 +55,17 @@
       <br>
       <hr>
       <br>
+      <h1 @click="showSwitch">Directives: v-show</h1>
+      <p v-show="showFlag">带有 v-show 的元素始终会被渲染并保留在 DOM 中。v-show 只是简单地切换元素的 CSS 属性 display。v-if 是“真正”的条件渲染，因为它会确保在切换过程中条件块内的事件监听器和子组件适当地被销毁和重建。v-if 也是惰性的：如果在初始渲染时条件为假，则什么也不做——直到条件第一次变为真时，才会开始渲染条件块。相比之下，v-show 就简单得多——不管初始条件是什么，元素总是会被渲染，并且只是简单地基于 CSS 进行切换。一般来说，v-if 有更高的切换开销，而 v-show 有更高的初始渲染开销。因此，如果需要非常频繁地切换，则使用 v-show 较好；如果在运行时条件很少改变，则使用 v-if 较好。</p>
+
+      <br>
+      <hr>
+      <h1>Directives: v-for</h1>
+      <ul>
+        <li v-for="(item, i) in items" :key="i">{{ item }}</li>
+        <li><hr></li>
+        <li v-for="(item, i) of items" :key="i">{{ item }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -67,7 +79,9 @@ export default {
       bindInput: "bind指令练习，你在此随意输入，观察下面文字变化 ...",
       count: 0,
       awesome: true,
-      loginType: "username"
+      loginType: "username",
+      showFlag: true,
+      items: [ "Foo", "Bar" ]
     };
   },
   methods: {
@@ -89,6 +103,9 @@ export default {
       } else {
         this.loginType = "username";
       }
+    },
+    showSwitch() {
+      this.showFlag = !this.showFlag
     }
   }
 };
